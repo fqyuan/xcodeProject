02970c599d0f972e9e247916d8fa05919d8590b3
fqyuan <fqyuan@fqyuandeMacBook-Pro.local>
2017-02-17 14:45:36 +0800

    Initial Commit

---
+++ OJ_Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OJ_Project
+//
+//  Created by fqyuan on 17/02/2017.
+//  Copyright © 2017 com.fqy.sort. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
